48f486b107a2b6a961ec86daa9b1be3c64d6f1a4
Chris Antreasian <chrisantreasian@gmail.com>
2017-09-24 20:39:52 -0400

    add module support

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -185,6 +185,7 @@ module.exports = {
                   loader: require.resolve('css-loader'),
                   options: {
                     importLoaders: 1,
+                    modules: true,
                     minimize: true,
                     sourceMap: true,
                   },
