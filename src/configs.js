81f3685f9062f2c4a9494ecb55f704669086c71e
Chris Antreasian <chrisantreasian@gmail.com>
2017-10-22 20:50:56 -0400

    reshuffle files, everything is broken but page renders

--- src/configs.js
+++
@@ -1,8 +0,0 @@
-
-export default {
-    baseurl: 'http://www.gigamegaultra.com/wp-json',
-    instagram: {
-        clientId:  'c95c3a716a844f658432378f5c13f547',
-        secret: 'e975ab9754a74b748d4109af1d381530'
-    }
-}
